c4f5b12546c897e90ce37cf49303189d2af524aa
Nasir-Jameel <beuroairer@gmail.com>
2026-06-19 12:07:24 -0700

    fourth commit

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ let createdUser = await userModel.create({
 res.redirect("/read");
 })
 
-app.get("/delete/:id"), async(req,res)=>{
+app.get("/delete/:id", async(req,res)=>{
     let users = await userModel.findOneAndDelete({_id: req.params.id})
     res.redirect("/read")
-}
+})
 app.listen(3000)
